58ee491df2424387bb22b1b3e159561f45ca2882
vimansha <vimanshamadushani88@gmail.com>
2024-01-18 14:46:52 +0530

    feat: second section

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -6,6 +6,11 @@ import SecondaryButton from "../Components/SecondaryButton";
 import img1 from "../Images/image-1.jpg";
 import rightIcon from "../Images/right-icon.png";
 import PrimaryButton from "../Components/PrimaryButton";
+import company1 from "../Images/company-1.jpg";
+import company2 from "../Images/company-2.jpg";
+import company3 from "../Images/company-3.jpg";
+import company4 from "../Images/company-4.jpg";
+import company5 from "../Images/company-5.jpg";
 
 export default function Homepage() {
   return (
@@ -70,6 +75,33 @@ export default function Homepage() {
             </div>
             <div className={`col-sm-6 ${classes.section1RightCol}`}>
               <img src={img1} alt="img1" className={classes.img1} />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={`container-fluid ${classes.secondSection}`}>
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <h5>Trusted by 150+ world's best companies</h5>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-2 col-md-4 col-sm-6">
+              <img src={company1} className={classes.companyLogo} />
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6">
+              <img src={company2} className={classes.companyLogo} />
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6">
+              <img src={company3} className={classes.companyLogo} />
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6">
+              <img src={company4} className={classes.companyLogo} />
+            </div>
+            <div className="col-lg-2 col-md-4 col-sm-6">
+              <img src={company5} className={classes.companyLogo} />
             </div>
           </div>
         </div>
